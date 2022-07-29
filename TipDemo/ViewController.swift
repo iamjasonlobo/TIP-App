edebe9a2e85819f50ede335ca65333bc8df92a3d
iamjasonlobo <jasonlobo.official@gmail.com>
2022-07-28 18:57:12 -0700

    Basic first screen UI and logic

--- TipDemo/ViewController.swift
+++ TipDemo/ViewController.swift
@@ -9,12 +9,40 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Declaring all UI components into usable form
+    @IBOutlet weak var billAmountTextField: UITextField! //input gotten by text field
+    @IBOutlet weak var tipAmountLabel: UILabel! //tip calculated
+    @IBOutlet weak var tipControl: UISegmentedControl! //tip option selected
+    @IBOutlet weak var totalLabel: UILabel! //total amount tip + bill amount
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
+    
+    @IBAction func tipControl(_ sender: Any) {
+        
+        //creating bill constant from text inputted - also adding optional value of 0 if input isnt proper
+        let bill = Double(billAmountTextField.text!) ?? 0
+        
+        // Creating array of percentages to be used for tip calculation
+        let tipPercentages = [0.15, 0.18, 0.20]
+        //creating tip value - using bill & multiplying by percentage getting index of selected Segment Index
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        
+        // Basic math operation
+        let total = bill + tip
+        
+        
+        // Display totalamount in form of String
+        tipAmountLabel.text = String(format: "%.2f", tip)
+        
+        // Display totalamount in form of String
+        totalLabel.text = String(format: "%.2f", total)
+        
+    }
+    
 
 }
 
