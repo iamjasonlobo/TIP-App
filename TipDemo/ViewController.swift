fcc5680e4d0fbb8d751570bf5c4258251fbecbc8
iamjasonlobo <jasonlobo.official@gmail.com>
2022-07-28 18:28:27 -0700

    Initial Commit

---
+++ TipDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TipDemo
+//
+//  Created by jlobo3 on 7/28/22.
+//  Copyright © 2022 Jason Lobo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
