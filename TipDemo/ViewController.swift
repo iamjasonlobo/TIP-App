adc26b475651335520d680c345681cf4b40c9de2
iamjasonlobo <jasonlobo.official@gmail.com>
2022-08-21 18:03:57 -0700

    Update changes and tweaks

--- TipDemo/ViewController.swift
+++ TipDemo/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
         //set title
-        self.title = "Tip Calculator"
+        self.title = "T!P"
     }
     
     @IBAction func tipControl(_ sender: Any) {
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
         let bill = Double(billAmountTextField.text!) ?? 0
         
         // Creating array of percentages to be used for tip calculation
-        let tipPercentages = [0.15, 0.18, 0.20]
+        let tipPercentages = [0.00,0.15, 0.18, 0.20]
         //creating tip value - using bill & multiplying by percentage getting index of selected Segment Index
         let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
         
@@ -39,10 +39,10 @@ class ViewController: UIViewController {
         
         
         // Display totalamount in form of String
-        tipAmountLabel.text = String(format: "%.2f", tip)
+        tipAmountLabel.text = String(format: "$%.2f", tip)
         
         // Display totalamount in form of String
-        totalLabel.text = String(format: "%.2f", total)
+        totalLabel.text = String(format: "$%.2f", total)
         
     }
     
